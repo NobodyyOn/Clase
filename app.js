304c78e7a03c5dd183e274276c0ffab2ab3d8628
Johan Steven <jjohan1533@gmail.com>
2024-10-30 13:28:14 -0500

    Se añadio app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// Variable que contiene una palabra
+let palabra = "Cama"
+
+switch (palabra) {
+    case "Cama":
+        console.log("Es una cama");
+        break;
+
+    default:
+        break;
+}
